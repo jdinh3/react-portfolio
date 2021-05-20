4ccc70ea3bc4c81813d76cd1f32da1ef46015c5b
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-05-19 20:38:51 -0500

    added about me section

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Col, Image } from "react-bootstrap";
+import "../components/About.css";
 
 function AboutMe() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about-section">
+      <div className="about-container">
+        <div className="my-image">
+          <Col xs={6} md={4}>
+            <Image src="./my-image.jpeg" width="300" roundedCircle />
+          </Col>
+        </div>
+        <div className="about-me">
+          <h4 className="about-header">Hello, I'm Jennifer Dinh!</h4>
+          <p>
+            I am a Full Stack Developer based in the Chicagoland area. I'm
+            currently open to new opportunities. At Georgia Tech, I have
+            collaborated with my classmates on full-stack applications where
+            reliability and communication were key components to achieving
+            successful applications. I enjoy working with others and using
+            creativity to problem-solve while coding.
+          </p>
+          <p>
+            In my free time, you can find me playing the guitar, songwriting,
+            and crocheting. I am excited for the opportunity to continue to grow
+            in this field, and to see what my individuality can bring to it!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
